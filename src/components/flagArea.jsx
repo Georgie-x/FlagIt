1de721516df605580601e0122b8caac00309ecab
Georgie <georgiex@gmail.com>
2025-08-06 13:51:25 +0100

    corrected assets and deployed to portofolio subdomain

--- src/components/flagArea.jsx
+++ src/components/flagArea.jsx
@@ -1,14 +1,13 @@
-
 import countryAlphaCodes from "../database"
+import worldFlags from "../assets/worldFlags.jpg"
 
 function FlagArea({ gameStage, country }) {
-
 	const flag = countryAlphaCodes[country]
 
 	return (
 		<div className='flag-area'>
 			{gameStage === 0 || gameStage === 11 ? (
-				<img src='src/assets/world-flags.jpg' alt='World Flags' />
+				<img src={worldFlags} alt='World Flags' />
 			) : flag ? (
 				<img src={`https://countryflagsapi.netlify.app/flag/${flag}.svg`} />
 			) : (
