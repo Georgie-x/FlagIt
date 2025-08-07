3bffc77e71a222ac00b3b4b5c81f16a156631817
lxanicl <S_O_A_D21@hotmail.com>
2025-08-07 18:01:31 +0100

    css clean up

--- src/components/instructions.jsx
+++ src/components/instructions.jsx
@@ -1,14 +1,12 @@
-import flagData from "../database";
-
 function Instructions() {
   return (
-    <>
+    <div className="instructions">
       <p>
         Can you guess all flags correctly? You'll be given 10 random flags of
         the world and have to guess the country it belongs to. There will be 4
         options to choose from, good luck.
       </p>
-    </>
+    </div>
   );
 }
 
