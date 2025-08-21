0b275967651589ffe88a5241e73bbcf996ccb399
Georgie <georgiex@gmail.com>
2025-08-21 16:44:26 +0100

    fixed api and flag position

--- src/components/flagArea.jsx
+++ src/components/flagArea.jsx
@@ -4,12 +4,17 @@ import worldFlags from "../assets/worldFlags.jpg"
 function FlagArea({ gameStage, country }) {
 	const flag = countryAlphaCodes[country]
 
+	console.log(flag)
+
 	return (
 		<div className='flag-area'>
 			{gameStage === 0 || gameStage === 11 ? (
 				<img src={worldFlags} alt='World Flags' />
 			) : flag ? (
-				<img src={`https://countryflagsapi.netlify.app/flag/${flag}.svg`} />
+				<img
+					src={`https://flagcdn.com/w320/${flag}.png
+`}
+				/>
 			) : (
 				<p>Flag not found</p>
 			)}
